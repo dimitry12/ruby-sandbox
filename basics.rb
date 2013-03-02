4d5e4f4107f8e00e5e502fb4437fb1e7a055d49f
Dimitry Pletnikov <dimitry@pletnikov.com>
2013-03-02 12:47:04 -0500

    strings quoting and interpolation;

--- basics.rb
+++ basics.rb
@@ -1,3 +1,22 @@
+puts "here is 2*2=#{2*2}" # double-quotes support expression interpolation
+
+# nice parsing!
+puts "and string is #{"object"*3}" # expression can have double-quited strings too
+
+puts %q{if we don't want to escape too many freak'g single-quotes} # this is single-quote
+puts %Q{and I said: "let's not escape"} # this is double-quote
+
+puts <<PERL_HEREDOC
+and Ruby has HEREDOC syntax
+so it's better than JavaScript
+and many other programming languages
+PERL_HEREDOC
+
+puts "to_s".to_s # to_s converts object to string
+
 myname = gets()
 
 puts "hello world and " + myname
+
+# let's not use single quote in Ruby, just as almost everywhere else
+puts 'hello world and ' + myname # performance benefit of single-quote is minimal
